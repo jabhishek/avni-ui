06c3894dbe8b78b5bb9c9fb6ee2cd3b550972991
Abhishek Jain <abhi2000@gmail.com>
2020-07-10 16:38:49 +0100

    another try to build on netlify

--- playground/.storybook/main.js
+++ playground/.storybook/main.js
@@ -20,11 +20,7 @@ module.exports = {
                 use: [
                     require.resolve('ts-loader'),
                     {
-                        loader: require.resolve('react-docgen-typescript-loader'), options: {
-                            // Provide the path to your tsconfig.json so that your stories can
-                            // display types from outside each individual story.
-                            tsconfigPath: path.resolve(__dirname, "../tsConfig.json"),
-                        },
+                        loader: require.resolve('react-docgen-typescript-loader')
                     },
                 ],
             }
